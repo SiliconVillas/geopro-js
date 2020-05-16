e132a6bd246e06788041f04f9d8e9bd46af78eb5
Michele Ursino <michele@micurs.com>
2020-05-15 17:24:55 -0700

    Add bundling using rollup

---
+++ rollup.config.js
@@ -0,0 +1,29 @@
+import typescript from 'rollup-plugin-typescript2';
+import json from '@rollup/plugin-json';
+
+import { terser } from 'rollup-plugin-terser';
+
+import pkg from './package.json';
+
+const esModule = {
+  file: pkg.module,
+  format: 'es',
+};
+const esmModule = {
+  file: pkg.esm,
+  format: 'esm',
+};
+const cjModule = {
+  file: pkg.main,
+  format: 'cjs',
+};
+
+export default {
+  input: 'src/index.ts',
+  output: [esModule, cjModule],
+  plugins: [typescript(), json(), terser()],
+  external: [
+    ...Object.keys(pkg.dependencies || {}),
+    ...Object.keys(pkg.peerDependencies || {}),
+  ],
+};
